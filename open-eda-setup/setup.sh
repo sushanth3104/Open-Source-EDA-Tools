111adb0a2365c12920553d40c8a73e42b83bd93e
sushanth3104 <sushkumarthaviti@gmail.com>
2025-09-19 14:26:40 +0530

    CHANGE SCRIPT DIR

--- open-eda-setup/setup.sh
+++ open-eda-setup/setup.sh
@@ -8,7 +8,7 @@ TOOLS_DIR="$BASE_DIR/tools"
 DOCS_DIR="$BASE_DIR/docs"
 
 echo "Setting up folder structure at $BASE_DIR ..."
-mkdir -p "$SCRIPTS_DIR" "$TOOLS_DIR" "$DOCS_DIR"
+mkdir -p "$TOOLS_DIR" "$DOCS_DIR"
 
 # Update package lists
 echo "Updating apt repositories..."
@@ -17,6 +17,5 @@ sudo apt-get upgrade -y
 
 echo "Setup complete!"
 echo "Directories created:"
-echo "   $SCRIPTS_DIR"
 echo "   $TOOLS_DIR"
 echo "   $DOCS_DIR"
